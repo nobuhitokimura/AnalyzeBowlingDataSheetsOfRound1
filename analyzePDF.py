980912e58e27d1174389d4a63766be7c7608bab1
nobuhitokimura <denosayuki.ms000@gmail.com>
2021-10-15 01:18:57 +0900

    [CHANGE] 抽出部分はテキストのみに変更

--- analyzePDF.py
+++ analyzePDF.py
@@ -1,3 +1,6 @@
+###
+### モジュール
+###
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
@@ -11,24 +14,34 @@ from pdfminer.layout import (
     LTTextLine,
 )
 
+#
+# PDF内のテキストを抽出する関数
+#
 def get_objs(layout, results):
     if not isinstance(layout, LTContainer):
         return
     for obj in layout:
         if isinstance(obj, LTTextLine):
-            results.append({'bbox': obj.bbox, 'text' : obj.get_text(), 'type' : type(obj)})
+            #results.append({'bbox': obj.bbox, 'text' : obj.get_text(), 'type' : type(obj)})
+            results.append(obj.get_text())
         get_objs(obj, results)
 
-def main(path):
+
+#
+# PDFを開いて解析するための関数
+#
+def getPDFtexts(path):
     with open(path, "rb") as f:
         parser = PDFParser(f)
         document = PDFDocument(parser)
         if not document.is_extractable:
             raise PDFTextExtractionNotAllowed
         # https://pdfminersix.readthedocs.io/en/latest/api/composable.html#
+
         laparams = LAParams(
             all_texts=True,
         )
+
         rsrcmgr = PDFResourceManager()
         device = PDFPageAggregator(rsrcmgr, laparams=laparams)
         interpreter = PDFPageInterpreter(rsrcmgr, device)
@@ -36,10 +49,11 @@ def main(path):
             interpreter.process_page(page)
             layout = device.get_result()
             results = []
-            print('objs-------------------------')
             get_objs(layout, results)
             for r in results:
                 print(r)
 
-main('score_sheet_20210929005339.pdf')
-#main('score_sheet_20210929005432.pdf')
+
+getPDFtexts('./pdfData/score_sheet_20210929005339.pdf')
+#main('./pdfData/score_sheet_20210929005432.pdf')
+
