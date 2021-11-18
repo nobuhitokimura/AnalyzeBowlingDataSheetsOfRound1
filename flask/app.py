544d346e03e70f3d6099a67a7bc8317e199792d2
nobuhitokimura <denosayuki.ms000@gmail.com>
2021-11-19 01:24:06 +0900

    [ADD]flask and sample codes

---
+++ flask/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+# Flaskクラスのインスタンス生成
+app = Flask(__name__) 
+
+@app.route('/') # URLを指定。URLにリクエストが来ると関数が実行される
+def index():
+    title = 'world'
+    framework = 'Flask'
+    mark = '!'
+    return render_template('index.html', title=title, framework=framework, mark=mark)
+
+if __name__ == '__main__':
+	app.run()
