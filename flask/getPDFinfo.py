37c5f0bcbacb749ce2a2a087c81d32a6446b9e4a
nobuhitokimura <denosayuki.ms000@gmail.com>
2021-12-18 21:16:55 +0900

    [ADD] 画像抽出

--- flask/getPDFinfo.py
+++ flask/getPDFinfo.py
@@ -11,7 +11,7 @@ def getPDFInfo(fileName):
     # PDFのファイルパス
     pdfFilePath = "./static/pdf/" + fileName
     # PDfから取得した画像を格納するフォルダ
-    pdfPicPath = "./static/pidPic"
+    pdfPicPath = "./static/pdfPic/"
     #os.makedirs(dirPath, exist_ok=True)
 
     # ファイルオープン
@@ -37,70 +37,35 @@ def getPDFInfo(fileName):
                     print(j, textBuf)
                     # 日時
                     if textCnt == 0:
-                        infoDic["year"] = textBuf[0:4]
-                        infoDic["month"] = textBuf[5:7]
-                        infoDic["day"] = textBuf[8:10]
-                        infoDic["dow"] = textBuf[11:12]
-                        infoDic["hour"] = textBuf[13:15]
-                        infoDic["minute"] = textBuf[16:18]
+                        infoDic['year'] = textBuf[0:4]
+                        infoDic['month'] = textBuf[5:7]
+                        infoDic['day'] = textBuf[8:10]
+                        infoDic['dow'] = textBuf[11:12]
+                        infoDic['hour'] = textBuf[13:15]
+                        infoDic['minute'] = textBuf[16:18]
                     # ユーザ名
                     elif textCnt == 1:
-                        infoDic["username"] = textBuf.split(' ')[0]
+                        infoDic['username'] = textBuf.split()[0]
                     # 店名、レーン番号、ゲーム番号
                     else:
                         textSl = textBuf.splitlines()
-                        infoDic["branch"] = textSl[0]
-                        infoDic["lane"] = textSl[1].split("番")[0]
-                        infoDic["gamenum"] = re.split('[第|ゲーム]', textSl[2])[1]
+                        infoDic['branch'] = textSl[0]
+                        infoDic['lane'] = textSl[1].split("番")[0]
+                        infoDic['gamenum'] = re.split('[第|ゲーム]', textSl[2])[1]
 
                     textCnt += 1
             
             print(infoList)
 
-'''
             # 画像取得
             for j, img in enumerate(page.getImageList()):
                 if j < (len(page.getImageList()) - 2):
                     x = scoreSheet.extractImage(img[0])
-                    name = os.path.join(os.getcwd(), folderName, f"{gameNum[j]}.{x['ext']}")
+                    picPath = os.path.join(pdfPicPath, f"{infoList[j]['gamenum']}.{x['ext']}")
                     
-                    print("name : ", gameNum[j])
-                    with open(name, "wb") as ofh:
+                    with open(picPath, "wb") as ofh:
                         ofh.write(x['image'])
-'''
 
-
-
-###
-### 対象のPDFの日時とゲーム数の情報を取得する関数
-###             
-def getGameNum(page):
-    gameNumBuf = []
-    dateBuf = ""
-    gameBuf = ""
-    for text in enumerate(page.getText('blocks')):
-        # 日時を取得
-        if text[0] % 4 == 1 and '（' in text[1][4]:
-            getDate = re.split('[/|（|）|:|\n]', text[1][4])
-            dateBuf = str(getDate[0]) + "-" + str(getDate[1]) + "-" + str(getDate[2]) + "_" + str(getDate[4]) + "-" + str(getDate[5])
-        
-        # ゲーム数の番がある配列の最後を取得, 保存するpng名を生成
-        buf = text[1][4].splitlines()
-        if len(buf) == 3:
-            gameBuf = str(re.split('[第|ゲーム]', buf[2])[1])
-            gameNumBuf.append(dateBuf + "_" + gameBuf)
-    return gameNumBuf
-
-###
-### ゲーム情報だけを取得する関数
-###
-def getGameInfo(page):
-    for text in enumerate(page.getText('blocks')):
-        buf = text[1][4].splitlines()
-
-        if (len(buf) > 1) or ((len(text[1][4]) - len(buf[0])) > 0):
-            if "Powered" not in buf[0]:
-                print(buf)
 
 # -----------------------------------------------------------------
 
