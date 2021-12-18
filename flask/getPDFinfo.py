d4ed24ed2a651680209a741b9a93d8aaeeab11f2
nobuhitokimura <denosayuki.ms000@gmail.com>
2021-12-18 21:03:48 +0900

    [ADD] 抽出したPDFテキストを各ゲームごとに辞書型で整理

--- flask/getPDFinfo.py
+++ flask/getPDFinfo.py
@@ -5,23 +5,59 @@ import re
 
 
 ###
-### PDFのテキストおよび画像を取得する関数
+### PDFのテキスト情報および画像を取得する関数
 ###
-def getPDFInfo(fileName, folderName):
+def getPDFInfo(fileName):
+    # PDFのファイルパス
+    pdfFilePath = "./static/pdf/" + fileName
     # PDfから取得した画像を格納するフォルダ
-    dirPath = os.path.dirname(fileName) + '/' + folderName
-    os.makedirs(dirPath, exist_ok=True)
+    pdfPicPath = "./static/pidPic"
+    #os.makedirs(dirPath, exist_ok=True)
 
     # ファイルオープン
-    with fitz.open(fileName) as scoreSheet:
+    with fitz.open(pdfFilePath) as scoreSheet:
         # 1ページごとに解析
         for i, page in enumerate(scoreSheet):
-            # テキスト抽出
-            #getGameInfo(page)
+            # テキスト情報抽出
+            infoList = []   # 1PDFのテキスト情報のリスト
+            infoDic = {}    # 1ゲーム分のテキスト情報の辞書型
+            textNum = len(page.get_text('blocks'))  # テキスト及び画像の総数
+            textCnt = 0 # 抽出した回数
+            for j, text in enumerate(page.get_text('blocks')):
+                if all([j!=0, j<textNum-2]):
+                    # 1ゲーム分の情報を取得した場合
+                    if textCnt == 3:
+                        infoList.append(infoDic)
+                        infoDic = {}
+                        textCnt = 0
+                        continue
+
+                    textBuf = text[4]
+                    print("----------")
+                    print(j, textBuf)
+                    # 日時
+                    if textCnt == 0:
+                        infoDic["year"] = textBuf[0:4]
+                        infoDic["month"] = textBuf[5:7]
+                        infoDic["day"] = textBuf[8:10]
+                        infoDic["dow"] = textBuf[11:12]
+                        infoDic["hour"] = textBuf[13:15]
+                        infoDic["minute"] = textBuf[16:18]
+                    # ユーザ名
+                    elif textCnt == 1:
+                        infoDic["username"] = textBuf.split(' ')[0]
+                    # 店名、レーン番号、ゲーム番号
+                    else:
+                        textSl = textBuf.splitlines()
+                        infoDic["branch"] = textSl[0]
+                        infoDic["lane"] = textSl[1].split("番")[0]
+                        infoDic["gamenum"] = re.split('[第|ゲーム]', textSl[2])[1]
+
+                    textCnt += 1
             
-            # ゲーム番号取得
-            gameNum = copy.copy(getGameNum(page))
-            
+            print(infoList)
+
+'''
             # 画像取得
             for j, img in enumerate(page.getImageList()):
                 if j < (len(page.getImageList()) - 2):
@@ -31,7 +67,7 @@ def getPDFInfo(fileName, folderName):
                     print("name : ", gameNum[j])
                     with open(name, "wb") as ofh:
                         ofh.write(x['image'])
-
+'''
 
 
 
@@ -69,22 +105,9 @@ def getGameInfo(page):
 # -----------------------------------------------------------------
 
 
-#getPDFInfo('../pdfData/score_sheet_20210929005339.pdf', 'pic')
-
-#dirPath = os.path.dirname(fileName) + '/' + folderName
-#os.makedirs(dirPath, exist_ok=True)
-
-fileName = "score_sheet_20211218161724.pdf"
-filePath = "./static/pdf/" + fileName
-
-# ファイルオープン
-with fitz.open(filePath) as scoreSheet:
-    # 1ページごとに解析
-    for i, page in enumerate(scoreSheet):
-        textNum = len(page.get_text('blocks'))
-        for j, text in enumerate(page.get_text('blocks')):
-            if all([j!=0, j<textNum-2]):
-                print("----------")
-                #print(text)
-                print(j, text[4])
-            
+#fileName = "score_sheet_20211218161724.pdf"
+fileName = "score_sheet_20211218161746.pdf"
+#fileName = "score_sheet_20211218161800.pdf"
+#fileName = "score_sheet_20211218161818.pdf"
+#fileName = "score_sheet_20211218161829.pdf"
+getPDFInfo(fileName)
