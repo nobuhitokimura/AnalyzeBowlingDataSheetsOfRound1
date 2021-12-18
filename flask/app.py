9a3eefd236d3f84f6e6aa1240bacfef80e1d26e4
nobuhitokimura <denosayuki.ms000@gmail.com>
2021-12-18 15:33:25 +0900

    (basic) htmlへ変数

--- flask/app.py
+++ flask/app.py
@@ -1,22 +1,14 @@
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
+# Flaskクラスのインスタンス生成
 app = Flask(__name__)
 
-# getのときの処理
-@app.route('/', methods=['GET'])
-def get():
-	return render_template('index.html', \
-		title = 'Form Sample(get)', \
-		message = '名前を入力して下さい。')
-
-# postのときの処理	
-@app.route('/', methods=['POST'])
-def post():
-	name = request.form['name']
-	return render_template('index.html', \
-		title = 'Form Sample(post)', \
-		message = 'こんにちは、{}さん'.format(name))
+@app.route('/') # URLを指定。URLにリクエストが来ると関数が実行される
+def index():
+    title = 'world'
+    framework = 'Flask'
+    mark = '!'
+    return render_template('index.html', title=title, framework=framework, mark=mark)
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
