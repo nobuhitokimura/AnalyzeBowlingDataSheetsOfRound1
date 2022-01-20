89b19c57cf45217fc2adbd6de50eb1db1766b61b
nobuhitokimura <denosayuki.ms000@gmail.com>
2022-01-21 04:28:30 +0900

    [Change] gunicorn

--- app.py
+++ app.py
@@ -311,4 +311,4 @@ def getGameCount():
 
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+	app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
